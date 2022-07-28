aebfe8ebc3374033ab03b2e3492ab3b111533c87
NeerajaAdi <adineeraja.kuslu@gmail.com>
2022-07-28 00:10:25 -0400

    added users list to the app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles     
+    end
+
+    def index
+        @user = User.all
+    end
+
     def new
         @user = User.new
     end
