8773d583ffd19249c4d081a1c8e51ee36f747583
NeerajaAdi <adineeraja.kuslu@gmail.com>
2022-07-28 14:44:05 -0400

    Created index to the users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles     
+        @articles = @user.articles   
     end
 
     def index
-        @user = User.all
+        @users = User.all
     end
 
     def new
